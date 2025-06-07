95a8448d5258ff932cb308d9cff25601ff548ed5
daniela.r <00113825@uca.edu.sv>
2025-06-07 15:52:04 -0600

    Update parcial2.cpp

--- parcial2.cpp
+++ parcial2.cpp
@@ -2,19 +2,30 @@
  using namespace std;
   int main (void) {
     //pidele al usuario que agrege las variables 
-    int i=1; j=10;
+    int i=1;
+    int (j)=1;
       for(int n=1;i<=5;i++){
          cout <<"suma"<<n<<":\n";
          cin>> i ;
 
 
-     }
+          int i=1; j=3;
+      for(int n=1;i>=5;i--){
+         cout <<"resta"<<n<<":\n";
+         cin>> j; 
+
+
+
+      }
 
 
 
 
     return 0;
+
+      }
   }
+
 
 
 
