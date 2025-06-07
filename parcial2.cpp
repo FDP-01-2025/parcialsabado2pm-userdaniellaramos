29624ef379a92f8a6b1debfc8ba80e383e27a9a9
daniela.r <00113825@uca.edu.sv>
2025-06-07 16:01:17 -0600

    Update parcial2.cpp

--- parcial2.cpp
+++ parcial2.cpp
@@ -1,46 +1,43 @@
-#include<iostream>
- using namespace std;
-  int main (void) {
-    //pidele al usuario que agrege las variables 
-    int i=1;
-    int (j)=1;
-      for(int n=1;i<=5;i++){
-         cout <<"suma"<<n<<":\n";
-         cin>> i ;
+#include <iostream>
 
+int main() {
+    int num1, num2, resultado;
+    char operacion;
 
-          int i=1; j=3;
-      for(int n=1;i>=5;i--){
-         cout <<"resta"<<n<<":\n";
-         cin>> j; 
+    // Pedir los números y la operación al usuario
+    std::cout << "Ingrese el primer número: ";
+    std::cin >> num1;
+    std::cout << "Ingrese el segundo número: ";
+    std::cin >> num2;
+    std::cout << "Ingrese la operación (+, -, *, /): ";
+    std::cin >> operacion;
 
-
-
-      }
-
-
-
+    // Realizar la operación según el switch
+    switch (operacion) {
+        case '+':
+            resultado = num1 + num2;
+            std::cout << "El resultado de la suma es: " << resultado << std::endl;
+            break;
+        case '-':
+            resultado = num1 - num2;
+            std::cout << "El resultado de la resta es: " << resultado << std::endl;
+            break;
+        case '*':
+            resultado = num1 * num2;
+            std::cout << "El resultado de la multiplicación es: " << resultado << std::endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                resultado = num1 / num2;
+                std::cout << "El resultado de la división es: " << resultado << std::endl;
+            } else {
+                std::cout << "Error: No se puede dividir por cero." << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Error: Operación no válida." << std::endl;
+            break;
+    }
 
     return 0;
-
-      }
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
