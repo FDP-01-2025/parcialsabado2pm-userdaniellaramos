d8d72c9da4d1432fc4659399bb889cca24254849
daniela.r <00113825@uca.edu.sv>
2025-06-07 15:40:21 -0600

    Add files via upload

---
+++ parcial2.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+ using namespace std;
+  int main (void) {
+    //pidele al usuario que agrege las variables 
+    int i=1; j=10;
+      for(int n=1;i<=5;i++){
+         cout <<"suma"<<n<<":\n";
+         cin>> i ;
+
+
+     }
+
+
+
+
+    return 0;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
